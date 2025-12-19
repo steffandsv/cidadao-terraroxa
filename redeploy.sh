93a52aa9abfd607dc4e7ea2c4fbdc493fb320360
Steffan Diorgy <steffanvernillo@gmail.com>
2025-12-19 11:08:25 -0300

    Implementação VPS

--- redeploy.sh
+++ redeploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 echo "[MABUS] Recebido gatilho de atualização..."
 cd /app
-# Garante keyscan
 mkdir -p /root/.ssh
 ssh-keyscan github.com >> /root/.ssh/known_hosts
 
-# Atualiza repo
 git fetch --all
 git reset --hard origin/main
 
-# Rebuild
-docker compose up -d --build web
+# Usando docker-compose (compatível com o binário do alpine)
+docker-compose up -d --build web
 echo "[MABUS] Deploy finalizado."
